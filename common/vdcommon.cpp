dc185029ff5ed5cba53881781dff4612575e0b4b
Frediano Ziglio <fziglio@redhat.com>
2019-11-29 20:34:41 +0000

    vdcommon: Do not use GetVersionEx to get Windows version
    GetVersionEx was deprecated by Microsoft in favour of utilities
    in versionhelpers.h.
    The new function returns the same results as:
    - GetVersionEx returns maximum 6.2 version and also 6.0 version
      is Vista, not Windows 7 so SYS_VER_WIN_7_CLASS was returned
      for all system from Windows Vista;
    - We don't support version earlier than Windows XP and no Windows
      versions had version >= 5.2 and < 6.0 so SYS_VER_WIN_XP_CLASS
      was returned from Windows XP;
    - we don't support any system before Windows XP.
    
    Tested under Windows XP, 7 and 10.
    
    Signed-off-by: Frediano Ziglio <fziglio@redhat.com>

--- common/vdcommon.cpp
+++ common/vdcommon.cpp
@@ -15,24 +15,18 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <stdarg.h>
-
 #include "vdcommon.h"
+
+#include <stdarg.h>
+#include <versionhelpers.h>
 
 SystemVersion supported_system_version()
 {
-    OSVERSIONINFOEX osvi;
-
-    ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
-    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-    if (!GetVersionEx((OSVERSIONINFO*)&osvi)) {
-        vd_printf("GetVersionEx() failed: %lu", GetLastError());
-        return SYS_VER_UNSUPPORTED;
-    }
-    if (osvi.dwMajorVersion == 5 && (osvi.dwMinorVersion == 1 || osvi.dwMinorVersion == 2)) {
-        return SYS_VER_WIN_XP_CLASS;
-    } else if (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion >= 0 && osvi.dwMinorVersion <= 2) {
+    if (IsWindowsVistaOrGreater()) {
         return SYS_VER_WIN_7_CLASS;
+    }
+    if (IsWindowsXPOrGreater()) {
+        return SYS_VER_WIN_XP_CLASS;
     }
     return SYS_VER_UNSUPPORTED;
 }
